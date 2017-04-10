0e28daa09d163e3d3173093a1b9b1e4a384021ba
qinbin <qinbin@xiaomi.com>
2017-04-10 09:54:03 +0800

    深度遍历

--- java/src/TreeTraversal.java
+++ java/src/TreeTraversal.java
@@ -44,4 +44,64 @@ public class TreeTraversal {
         return list;
     }
 
+    //左右都访问完了再访问根节点
+    public static void postOrderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        postOrderTraversal(root.left, list);
+        postOrderTraversal(root.right, list);
+        list.add(root.val);
+    }
+
+    public static List<Integer> postOrderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode p = root;
+        do {
+            while (p != null) {
+                stack.push(p);
+                p = p.left;
+            }
+            TreeNode q = null;//记录刚访问过的节点
+            while (!stack.isEmpty()) {
+                p = stack.pop();
+                if (p.right == q) {
+                    //p可以出栈了
+                    list.add(p.val);
+                    q = p;
+                } else {
+                    //p的右节点还没有访问，归栈
+                    stack.push(p);
+                    //处理右子树
+                    p = p.right;
+                    break;
+                }
+            }
+        } while (!stack.isEmpty());
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode();
+        root.val = 1;
+        root.left = new TreeNode();
+        root.left.val = 2;
+        root.right = new TreeNode();
+        root.right.val = 3;
+        root.left.right = new TreeNode();
+        root.left.right.val = 4;
+        List<Integer> list = new ArrayList<>();
+        postOrderTraversal(root, list);
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+        System.out.println("----------------");
+        list = postOrderTraversal(root);
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+    }
+
+
 }
